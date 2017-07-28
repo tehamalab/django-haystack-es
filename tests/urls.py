6b6c3540ed97214b22d231176295f92056abca29
Emil <bmachaku@gmail.com>
2017-07-28 10:16:34 +0300

    fix tests, remove non-existing patterns

--- tests/urls.py
+++ tests/urls.py
@@ -3,8 +3,5 @@ from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import url, include
 
-from haystack_es.urls import urlpatterns as haystack_es_urls
 
-urlpatterns = [
-    url(r'^', include(haystack_es_urls, namespace='haystack_es')),
-]
+urlpatterns = []
